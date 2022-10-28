1b3310161f1f842319bf480c9f34243e70f7100c
Herwig Frans <herwig_frans@hotmail.com>
2022-10-28 10:08:34 +0200

    Add Item integration test added

--- src/test/java/com/switchfully/eurder/items/AddItemIntegrationTest.java
+++ src/test/java/com/switchfully/eurder/items/AddItemIntegrationTest.java
@@ -1,7 +1,5 @@
-package com.switchfully.eurder.users;
+package com.switchfully.eurder.items;
 
-import com.switchfully.eurder.users.domain.NewMemberDto;
-import com.switchfully.eurder.users.domain.ReturnMemberDto;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
@@ -10,30 +8,30 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserIntegrationTests {
+public class AddItemIntegrationTest {
 
     @LocalServerPort
     private int port;
 
     @Test
-    void AddNewMember_HappyPath() {
-
-        String body = "{\"firstName\":\"Spongebob\",\"lastName\":\"Squarepants\",\"emailAddress\":\"Squarepants@hotmail.com\",\"city\":\"BikiniBottom\",\"postalCode\":\"1000\",\"streetName\":\"waterstreet\",\"houseNumber\":\"1\",\"additionalInfo\":\"string\",\"phoneNumber\":\"string\",\"password\":\"string\"}";
+    void addNewItemByAdmin_HappyPath() {
+        String body = "{\"name\":\"flour\",\"description\":\"crushedgrain\",\"price\":\"15.00\",\"amount\":1}";
 
         RestAssured
                 .given()
+                .auth()
+                .preemptive()
+                .basic("admin", "password")
                 .baseUri("http://localhost")
                 .port(port)
                 .when()
                 .accept(ContentType.JSON)
                 .body(body)
                 .contentType(ContentType.JSON)
-                .post("/members")
+                .post("/items")
                 .then()
                 .assertThat()
                 .statusCode(HttpStatus.CREATED.value());
-
     }
-
 
 }

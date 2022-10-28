dfe4d3fb720aa5c4c4aee2de24cec13e3766a2af
Herwig Frans <herwig_frans@hotmail.com>
2022-10-28 16:04:47 +0200

    added itemGroupService for better structure

---
+++ src/main/java/com/switchfully/eurder/orders/service/ItemGroupService.java
@@ -0,0 +1,33 @@
+package com.switchfully.eurder.orders.service;
+
+import com.switchfully.eurder.items.domain.ItemRepository;
+import com.switchfully.eurder.orders.domain.OrderItemDto;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Service
+public class ItemGroupService {
+    private final int DAYS_TO_SHIP_WHEN_ITEM_NOT_IN_STOCK = 7;
+    private final int DAYS_TO_SHIP_WHEN_IN_STOCK = 1;
+    private ItemRepository itemRepository;
+
+    public ItemGroupService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+    public LocalDate calculateShippingDate(OrderItemDto orderItemDto) {
+        int itemAmount = getItemAmount(orderItemDto);
+        if (itemAmount - orderItemDto.getAmount() <= 0) {
+            return LocalDate.now().plusDays(DAYS_TO_SHIP_WHEN_ITEM_NOT_IN_STOCK);
+        }
+        return LocalDate.now().plusDays(DAYS_TO_SHIP_WHEN_IN_STOCK);
+    }
+    private int getItemAmount(OrderItemDto orderItemDto) {
+        return itemRepository.getItems().get(orderItemDto.getName()).getAmount();
+    }
+    public BigDecimal calculateItemGroupPrice(OrderItemDto orderItemDto) {
+        BigDecimal itemPrice = itemRepository.getItems().get(orderItemDto.getName()).getPrice();
+        return itemPrice.multiply(BigDecimal.valueOf(orderItemDto.getAmount()));
+    }
+}
--- src/main/java/com/switchfully/eurder/orders/service/OrderService.java
+++ src/main/java/com/switchfully/eurder/orders/service/OrderService.java
@@ -1,23 +1,19 @@
 package com.switchfully.eurder.orders.service;
 
-import com.switchfully.eurder.items.domain.Item;
-import com.switchfully.eurder.items.domain.ItemRepository;
 import com.switchfully.eurder.orders.domain.*;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class OrderService {
-    private final int DAYS_TO_SHIP_WHEN_ITEM_NOT_IN_STOCK = 7;
-    private final int DAYS_TO_SHIP_WHEN_IN_STOCK = 1;
-    private OrderMapper orderMapper = new OrderMapper();
-    private ItemRepository itemRepository;
 
-    public OrderService(ItemRepository itemRepository) {
-        this.itemRepository = itemRepository;
+    private OrderMapper orderMapper = new OrderMapper();
+    private ItemGroupService itemGroupService;
+
+    public OrderService(ItemGroupService itemGroupService) {
+        this.itemGroupService = itemGroupService;
     }
 
 
@@ -33,32 +29,14 @@ public class OrderService {
                 new ItemGroup(
                         orderItemDto.getName(),
                         orderItemDto.getAmount(),
-                        calculateShippingDate(orderItemDto),
-                        calculateItemGroupPrice(orderItemDto))));
+                        itemGroupService.calculateShippingDate(orderItemDto),
+                        itemGroupService.calculateItemGroupPrice(orderItemDto))));
     }
 
     private BigDecimal calculateTotalPrice(List<ItemGroup> itemGroupList) {
         return itemGroupList.stream()
                 .map((itemGroup -> itemGroup.getItemGroupPrice()))
                 .reduce(BigDecimal.valueOf(0), (itemPrice1, itemprice2) -> itemPrice1.add(itemprice2));
-    }
-
-    private LocalDate calculateShippingDate(OrderItemDto orderItemDto) {
-        int itemAmount = getItemAmount(orderItemDto);
-        if (itemAmount - orderItemDto.getAmount() <= 0) {
-            return LocalDate.now().plusDays(DAYS_TO_SHIP_WHEN_ITEM_NOT_IN_STOCK);
-        }
-        return LocalDate.now().plusDays(DAYS_TO_SHIP_WHEN_IN_STOCK);
-    }
-
-
-    private int getItemAmount(OrderItemDto orderItemDto) {
-        return itemRepository.getItems().get(orderItemDto.getName()).getAmount();
-    }
-
-    private BigDecimal calculateItemGroupPrice(OrderItemDto orderItemDto) {
-        BigDecimal itemPrice = itemRepository.getItems().get(orderItemDto.getName()).getPrice();
-        return itemPrice.multiply(BigDecimal.valueOf(orderItemDto.getAmount()));
     }
 
 }
